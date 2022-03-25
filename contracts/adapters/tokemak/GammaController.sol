39c1e73a4ba8c03f68b6cdaa0d6d93b28d2b2e67
xyz-cmd <86979474+xyz-cmd@users.noreply.github.com>
2022-03-25 17:29:23 -0400

    Update GammaController.sol

--- contracts/adapters/tokemak/GammaController.sol
+++ contracts/adapters/tokemak/GammaController.sol
@@ -69,21 +69,20 @@ contract GammaController is BaseController {
     /// @param token1 address of pool token1
     /// @param fee fee of pool
     /// @param amount Quantity of LP tokens to burn in the withdrawal
-    /// @param minAmounts min amount of token0, token1 to receive after LP burn 
+    /// @param minAmounts min amount of token0, token1 to receive after LP burn
     function withdraw(
         address token0,
         address token1,
         uint24 fee,
         uint256 amount,
         uint256[N_COINS] memory minAmounts,
-        uint256[N_COINS] memory minBurnAmounts
     ) external onlyManager {
         
         address lpTokenAddress = hypeFactory.getHypervisor(token0, token1, fee);
         uint256 lpTokenBalanceBefore = IERC20(lpTokenAddress).balanceOf(manager);
         uint256[N_COINS] memory coinsBalancesBefore = _getCoinsBalances(lpTokenAddress);
 
-        ITokeHypervisor(lpTokenAddress).withdraw(amount, manager, manager, minBurnAmounts[0], minBurnAmounts[1]);
+        ITokeHypervisor(lpTokenAddress).withdraw(amount, manager, manager, minAmounts[0], minAmounts[1]);
 
         uint256 lpTokenBalanceAfter = IERC20(lpTokenAddress).balanceOf(manager);
         uint256[N_COINS] memory coinsBalancesAfter = _getCoinsBalances(lpTokenAddress);
